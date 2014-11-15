2ad326c2dcd979f3bda74b9c391b7e2f3cd7bc83
god2g <370567301@qq.com>
2014-11-15 15:16:12 +0800

    Project Created

---
+++ MvcShopping/Models/ProductCategory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcShopping.Models
+{
+    public class ProductCategory
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [DisplayName]
+        [Required(ErrorMessage="请输入商品名称")]
+        [MaxLength(20,ErrorMessage="商品类别不能超过20个字")]
+        public string Name { get; set; }
+    }
+}
